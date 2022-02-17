f128afaaa606cac7596845f348a2c9a658bb5377
Macey <61588664+Achaz021@users.noreply.github.com>
2022-02-17 10:08:45 +0000

    define app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_app.datadic import group26_database_dictionary
-from flask_app.tests import run_tests
 
-# 01. DEFINE APP
+# DEFINE APP
 
 app = Flask(__name__)
 
-# 02.DEFINE DATABASE CONNECTION
+# DEFINE DATABASE CONNECTION
 
 myhost = group26_database_dictionary['endpoint']
 myuser = group26_database_dictionary['username']
@@ -18,8 +17,3 @@ mydb = group26_database_dictionary['database']
 
 if __name__ == "__main__":
     print('Running')
-    test_result = run_tests(host=myhost, user=myuser, password=mypassword, port=myport, db=mydb)
-
-    # Check if the python test cases run alright
-    if test_result:
-        app.run(debug=True, port=5000)
