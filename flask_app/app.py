314837e838dbfa1105df9b2d47442cd958fba4c0
Becominging <1312545407@qq.com>
2022-04-13 23:14:33 +0100

    merge predictions

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import sqlalchemy as sqla
 from flask import Flask, render_template
-from datadic_sql import *
+import json
+import pickle
+import numpy
+
+
+# Dictionary for convenience
+database_info = {'username': 'group26',
+                 'password': '26group1',
+                 'database': 'dbikes',
+                 'host': 'dbbikes.ccllddmzhx5q.us-east-1.rds.amazonaws.com',
+                 'port': '3306'}
 
 # Define app
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 
 # Connect to a database engine
@@ -45,7 +55,6 @@ def near():
     return render_template('nearest.html')
 
 
-
 @app.route("/stations")
 def stations():
     """Returns the station Json Data"""
@@ -79,7 +88,6 @@ def stations():
     return data_json
 
 
-# noinspection PyCallingNonCallable
 @app.route('/hourly/<int:station_number>')
 def hourly(station_number):
     """Returns the hourly Json Data"""
@@ -185,11 +193,11 @@ def current_weather():
 
     return data_json
 
-@app.route('/forecast_weather/<int:station_number>')
-def forecast_weather(station_number):
-    """Returns the current weather Json Data of Dublin"""
+@app.route("/weather_forecast")
+def weather_forecast():
+    """Returns the weather forecast Json Data of Dublin"""
 
-    print("forecast_weather() in operation...\n")
+    print("weather_forecast() in operation...\n")
 
     try:
         # Connect to the RDS database
@@ -199,13 +207,17 @@ def forecast_weather(station_number):
                                    port=database_info['port'],
                                    db=database_info['database'], )
 
-        sql_statement = "SELECT temp as temp FROM forecast  " \
-                        "EXTRACT(HOUR FROM from_unixtime(forecast_time_dt)) as Hourly " \
-                        "GROUP BY EXTRACT(HOUR FROM from_unixtime(forecast_time_dt)) " \
-                        "ORDER BY EXTRACT(HOUR FROM from_unixtime(forecast_time_dt)) asc"
+        sql_statement = "SELECT f.number,f.main," \
+                        "avg(f.feels_like) as Avg_temp," \
+                        "avg(f.wind_speed) as Avg_wind_speed," \
+                        "date(from_unixtime(f.forecast_time_dt)) as Daily " \
+                        "FROM forecast as f " \
+                        "WHERE f.number = 2 " \
+                        "GROUP BY date(from_unixtime(f.forecast_time_dt)) " \
+                        "ORDER BY date(from_unixtime(f.forecast_time_dt)) desc"
 
 
-        df = pd.read_sql(sql_statement, engine, params={'station_id': station_number})
+        df = pd.read_sql(sql_statement, engine)
         # Turn the data into the json
         data_json = df.to_json(orient="records")
 
@@ -215,6 +227,75 @@ def forecast_weather(station_number):
     print("forecast_weather() finish!\n\n")
 
     return data_json
+
+
+
+@app.route('/prediction/<int:station_number>')
+def predict(station_number):
+    print("predict() in operation...\n")
+
+    # Connect to the RDS database
+    engine = connect_db_engine(host=database_info['host'],
+                               user=database_info['username'],
+                               password=database_info['password'],
+                               port=database_info['port'],
+                               db=database_info['database'])
+
+    sql_statement1 = "SELECT s.number, s.name, s.address, s.bike_stands, s.position_lat, " \
+                    "s.position_long, a.available_bike_stands, a.available_bikes, " \
+                    "MAX(from_unixtime(a.last_update)) AS 'last_update_time', a.created_date AS 'created_date' " \
+                    "FROM availability as a " \
+                    "INNER JOIN station as s " \
+                    "ON s.number = a.number " \
+                    "WHERE s.number = %(station_id)s " \
+                    "GROUP BY s.number " \
+                    "ORDER BY s.number;"
+    df1 = pd.read_sql(sql_statement1, engine, params = {'station_id':station_number})
+
+    sql_statement2 = "SELECT f.number,f.main," \
+                     "avg(f.temp) as Avg_temp," \
+                     "avg(f.pressure) as Avg_pressure," \
+                     "avg(f.humidity) as Avg_humidity," \
+                     "avg(f.visibility) as Avg_visibility," \
+                     "avg(f.wind_speed) as Avg_wind_speed," \
+                     "avg(f.wind_degree) as Avg_wind_degree," \
+                     "avg(f.clouds_all) as Avg_clouds_all," \
+                     "date(from_unixtime(f.forecast_time_dt)) as Daily " \
+                    "FROM forecast as f " \
+                    "WHERE f.number = 2 " \
+                    "GROUP BY date(from_unixtime(f.forecast_time_dt)) " \
+                    "ORDER BY date(from_unixtime(f.forecast_time_dt)) desc"
+
+    df2 = pd.read_sql(sql_statement2, engine)
+    f = open('rfc2.pickle', 'rb')
+    rfc1 = pickle.load(f)
+    f.close()
+    prediction_list = []
+    for i in [3,2,1,0]:
+        availability_prediction = rfc1.predict([[station_number,df1["bike_stands"],df1["position_lat"],df1["position_long"],0.965926,0.258819,df2.loc[i:i,"Avg_temp"],df2.loc[i:i,"Avg_pressure"],df2.loc[i:i,"Avg_humidity"],df2.loc[i:i,"Avg_visibility"],df2.loc[i:i,"Avg_wind_speed"],df2.loc[i:i,"Avg_wind_degree"],df2.loc[i:i,"Avg_clouds_all"],True]])
+        print(availability_prediction)
+        prediction_list.append(availability_prediction[0])
+    print(prediction_list)
+    prediction_dict = {"Day1": prediction_list[0],"Day2":prediction_list[1],"Day3":prediction_list[2],"Day4":prediction_list[3]}
+    print(prediction_dict)
+    result = json.dumps(prediction_dict, cls=NpEncoder)
+
+    print("predict() finish!\n\n")
+
+
+    return result
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (numpy.int_, numpy.intc, numpy.intp, numpy.int8,
+            numpy.int16, numpy.int32, numpy.int64, numpy.uint8,
+            numpy.uint16,numpy.uint32, numpy.uint64)):
+            return int(obj)
+        elif isinstance(obj, (numpy.float_, numpy.float16, numpy.float32, numpy.float64)):
+            return float(obj)
+        elif isinstance(obj, (numpy.ndarray,)): # add this line
+            return obj.tolist() # add this line
+        return json.JSONEncoder.default(self, obj)
 
 # Run
 if __name__ == "__main__":
